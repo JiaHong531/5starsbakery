1e09b09cc0d382e0b528c9c50d96bb4ab6514f2b
Ollie126328 <chengruisaw@gmail.com>
2025-12-11 00:51:40 +0800

    feat: Implement Search Bar, Shopping Cart, and Cart Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,24 +8,31 @@ import AdminLogin from './pages/AdminLogin';
 import Terms from './pages/Terms';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import About from './pages/About';
+
+import { SearchProvider } from './context/SearchContext';
+import { CartProvider } from './context/CartContext';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/admin-login" element={<AdminLogin />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/about" element={<About />} />
-          {/* Add other routes here */}
-        </Routes>
-      </Layout>
-    </Router>
+    <SearchProvider>
+      <CartProvider>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/admin-login" element={<AdminLogin />} />
+              <Route path="/terms" element={<Terms />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="/about" element={<About />} />
+              {/* Add other routes here */}
+            </Routes>
+          </Layout>
+        </Router>
+      </CartProvider>
+    </SearchProvider>
   );
 }
 
